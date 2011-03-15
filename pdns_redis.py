b636d2385a301d1e28d79d3cc2e54d18569bc93d
Bjarni R. Einarsson <bre@beanstalks-project.net>
2011-03-15 14:12:20 +0000

    Clarifying license and copyright

--- pdns_redis.py
+++ pdns_redis.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python
 # 
-# pdns-redis.py, Copyright 2010, Bjarni R. Einarsson, http://bre.klaki.net/
+# pdns-redis.py, Copyright 2011, Bjarni R. Einarsson <http://bre.klaki.net/>
+#                                and The Beanstalks Project ehf.
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at
+# your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
 #
-# You should have received a copy of the GNU Affero General Public License
+# You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 BANNER = "pdns-redis.py, by Bjarni R. Einarsson"
 DOC = """\
-pdns-redis.py is Copyright 2010, Bjarni R. Einarsson, http://bre.klaki.net/
+pdns-redis.py is Copyright 2011, Bjarni R. Einarsson, http://bre.klaki.net/
+                                 and The Beanstalks Project ehf.
 
 This program implements a PowerDNS pipe-backend for looking up domain info in a
 Redis database.  It also includes basic CLI functionality for querying, setting
